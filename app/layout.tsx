332bd6ab119018d0e725aa29c853e10230832ab1
ShrootBuck <ShrootBuck@users.noreply.github.com>
2025-02-09 14:05:31 -0700

    Remove Posthog

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import { Inter } from "next/font/google";
-import { CSPostHogProvider } from "./providers";
 
 // If loading a variable font, you don't need to specify the font weight
 const inter = Inter({ subsets: ["latin"] });
@@ -18,9 +17,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <CSPostHogProvider>
-        <body className={`${inter.className} antialiased`}>{children}</body>
-      </CSPostHogProvider>
+      <body className={`${inter.className} antialiased`}>{children}</body>
     </html>
   );
 }
